62acc0958bae3ce6dd5ec06bcfde26f48443e594
RadosavAntonio <radosav.antonio@gmail.com>
2025-09-17 13:26:33 +0100

    final

--- src/store/hooks/useAppSelector.ts
+++ src/store/hooks/useAppSelector.ts
@@ -1,6 +1,4 @@
-import { useSelector } from 'react-redux'
+import { TypedUseSelectorHook, useSelector } from 'react-redux'
 import type { RootState } from '../store'
 
-export const useAppSelector = <TSelected = unknown>(
-  selector: (state: RootState) => TSelected,
-): TSelected => useSelector(selector)
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
